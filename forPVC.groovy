cdedce04f35d56e827372ed340d0971faa884463
ad1thyaaa <aditkrishna95@gmail.com>
2026-03-17 22:15:26 +0530

    modified jenkin script

--- forPVC.groovy
+++ forPVC.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git 'https://github.com/ad1thyaaa/DevOPSw.git'
+                Checkout SCM
             }
         }
 
